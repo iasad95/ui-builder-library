b6768f4f351c2569f9288d3d71db3b988633f034
asad <asad@local>
2023-07-14 18:23:04 +0500

    feat(circle-progress): add component logic and module wiring

---
+++ projects/ui-builder-component-lib/src/lib/components/circle-progress/circle-progress.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CircleProgressComponent } from './circle-progress.component';
+@NgModule({
+  declarations: [CircleProgressComponent],
+  imports: [CommonModule],
+  exports: [CircleProgressComponent],
+})
+export class CircleProgressModule {}
