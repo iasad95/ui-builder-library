f6244eaf9aff99af99d070ce970b0f598ab431a0
asad <asad@local>
2024-04-23 08:10:55 +0500

    feat(multi-colored-text): add component logic and module wiring

---
+++ projects/ui-builder-component-lib/src/lib/components/multi-colored-text/multi-colored-text.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MultiColoredTextComponent } from './multi-colored-text.component';
+@NgModule({
+  declarations: [MultiColoredTextComponent],
+  imports: [CommonModule],
+  exports: [MultiColoredTextComponent],
+})
+export class MultiColoredTextModule {}
