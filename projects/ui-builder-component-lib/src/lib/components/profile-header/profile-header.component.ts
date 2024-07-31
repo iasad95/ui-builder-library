4c7d0ac9a39133fc4e69679b1c95c3eee4213436
asad <asad@local>
2024-07-31 10:08:17 +0500

    feat(profile-header): add component logic and module wiring

---
+++ projects/ui-builder-component-lib/src/lib/components/profile-header/profile-header.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { ProfileHeaderOrientation } from '../../enums/orientation.enum';
+@Component({
+  selector: 'lib-profile-header',
+  templateUrl: './profile-header.component.html',
+  styleUrls: ['./profile-header.component.scss'],
+})
+export class ProfileHeaderComponent {
+  @Input() photo: string;
+  @Input() name: string;
+  @Input() leftIcon: any;
+  @Input() rightIcon: any;
+  @Input() leftText: string;
+  @Input() rightText: string;
+  @Input() orientation: string = ProfileHeaderOrientation.horizontal;
+}
