01417445f8ae543f0af51948c8f441bc2a99d018
asad <asad@local>
2025-05-13 09:01:28 +0500

    feat(demo/menu-item): introduce component demo flow

---
+++ projects/ui-builder-component-lib-test/src/app/components/menu-item-demo/menu-item-demo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-item-demo',
+  templateUrl: './menu-item-demo.component.html',
+  styleUrls: ['./menu-item-demo.component.scss'],
+})
+export class MenuItemDemoComponent {}
