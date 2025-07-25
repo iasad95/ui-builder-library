0edbd4ebc9767060acd88818fdce3361a19ba92e
asad <asad@local>
2025-07-25 15:17:46 +0500

    feat(demo/profile-header): add interactive showcase behavior

---
+++ projects/ui-builder-component-lib-test/src/app/components/profile-header/profile-header.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { ProfileHeaderOrientation } from 'ui-builder-component-lib';
+
+@Component({
+  selector: 'app-profile-header',
+  templateUrl: './profile-header.component.html',
+  styleUrls: ['./profile-header.component.scss'],
+})
+export class ProfileHeaderComponent {
+  public client = {
+    id: 1,
+    name: 'James Sullivan',
+    value: 21,
+    photo: 'https://i.ibb.co/Cv0FvRb/unsplash-WNo-Ln-Jo7t-S8profile.png',
+    notification: 10,
+    age: 23,
+    address: 'New York City, NY',
+    myPhotos: [
+      'https://i.ibb.co/Cv0FvRb/unsplash-WNo-Ln-Jo7t-S8profile.png',
+      'https://i.ibb.co/Cv0FvRb/unsplash-WNo-Ln-Jo7t-S8profile.png',
+      'https://i.ibb.co/Cv0FvRb/unsplash-WNo-Ln-Jo7t-S8profile.png',
+    ],
+  };
+
+  public progressbarCode: string = `<lib-profile-header
+    [orientation]="orientation"
+    [photo]="photo"
+    [name]="name"
+    [leftIcon]="leftIconPath"
+    [leftText]="leftText"
+    [rightIcon]="rightIconPath"
+    [rightText]="rightText"
+  >
+  </lib-profile-header>`;
+
+  public icons: any = {
+    hash: 'assets/icon/hash.svg',
+    location: 'assets/icon/location.svg',
+  };
+
+  public vertical = ProfileHeaderOrientation.vertical;
+  public horizontal = ProfileHeaderOrientation.horizontal;
+}
