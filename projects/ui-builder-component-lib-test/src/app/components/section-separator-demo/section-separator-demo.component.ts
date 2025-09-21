2785336800f9800284a8d5b6251ef2aad86dba34
asad <asad@local>
2025-09-21 18:47:48 +0500

    feat(demo/section-separator): introduce component demo flow

---
+++ projects/ui-builder-component-lib-test/src/app/components/section-separator-demo/section-separator-demo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-separator-demo',
+  templateUrl: './section-separator-demo.component.html',
+  styleUrls: ['./section-separator-demo.component.scss'],
+})
+export class SectionSeparatorDemoComponent {
+  public sectionSeparatorCode: string = '<lib-section-separator></lib-section-separator>';
+}
