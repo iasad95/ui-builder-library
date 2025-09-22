d942741d09dcc4ab2784ebee89f7177e719b0ffe
asad <asad@local>
2025-09-22 12:51:38 +0500

    feat(demo/select-option): introduce component demo flow

---
+++ projects/ui-builder-component-lib-test/src/app/components/select-option-demo/select-option-demo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-option-demo',
+  templateUrl: './select-option-demo.component.html',
+  styleUrls: ['./select-option-demo.component.scss'],
+})
+export class SelectOptionDemoComponent {}
